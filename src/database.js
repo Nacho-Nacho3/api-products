731a428a0b80bba91ce94ace2b3e3c00a02ddbe6
JoaquinDesk <joaquinignacio862@gmail.com>
2026-05-28 14:10:02 -0600

    fix: puerto dinamico y variables de entorno

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
+import 'dotenv/config'
 
-mongoose.connect("mongodb+srv://elkkasfoxxtt:lucas@cluster-nacho.v4vvghs.mongodb.net/Groceries_Nacho?appName=Cluster-nacho")
-.then((cnn)=> console.log ("conected to MongoDB"))
-.catch((error)=> console.error("Error connecting to MongoDB",error));
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("conected to MongoDB"))
+.catch((error) => console.error("Error connecting to MongoDB", error));
 
 export default mongoose;
